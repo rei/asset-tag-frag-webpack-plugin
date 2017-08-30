efe7ff24583d644f2581df97557198a8fcfc65fb
Alex Perkins <aperkin@rei.com>
2017-08-30 10:37:16 -0700

    Adding functionality to prepend path to the tag's asset.

--- src/lib.js
+++ src/lib.js
@@ -26,20 +26,31 @@ const createLib = function createLib(opts) {
     }, '').trim();
   };
 
-  // Helpers fns to get tagProps data.
+  /**
+   * Helpers fns to get tagProps data.
+   * @param type The property type (js | css)
+   */
   const getTagProps = type => R.pathOr({}, [type, 'tagProps'], options);
 
   /**
-     * Creates the snippet of html needed for a built asset.
-     * @param {*} assetName The filename of the js/css asset.
-     */
+   * Helpers fns to get prependPath for an asset type.
+   * @param type The property type (js | css)
+   */
+  const getPrependPath = type => R.pathOr('', [type, 'prependPath'], options);
+
+  /**
+   * Creates the snippet of html needed for a built asset.
+   * @param assetName The filename of the js/css asset.
+   * @returns {{ext: *, tag: string}}
+   */
   const createAssetTag = (assetName) => {
     const ext = path.extname(assetName);
     const tagPropsJs = getTagProps('js');
     const tagPropsCss = getTagProps('css');
 
     if (ext === '.js') {
-      const tagPropsString = `src="${assetName}" ${objToString(tagPropsJs)}`.trim();
+      const srcPath = path.join(getPrependPath('js'), assetName);
+      const tagPropsString = `src="${srcPath}" ${objToString(tagPropsJs)}`.trim();
       return {
         ext,
         tag: `<script ${tagPropsString}></script>`,
@@ -47,7 +58,8 @@ const createLib = function createLib(opts) {
     }
 
     // Otherwise, css.
-    const tagPropsString = `rel="stylesheet" type="text/css" href="${assetName}" ${objToString(tagPropsCss)}`.trim();
+    const srcPath = path.join(getPrependPath('css'), assetName);
+    const tagPropsString = `rel="stylesheet" type="text/css" href="${srcPath}" ${objToString(tagPropsCss)}`.trim();
 
     return {
       ext,
@@ -75,6 +87,9 @@ const createLib = function createLib(opts) {
 
   const filename = ext => R.pathOr(`assets.${ext}.html`, [ext, 'filename'], options);
 
+  /**
+   * Create the new asset object with api webpack expects.
+   */
   const createWebpackAsset = R.curry((type, source) => {
     const asset = {};
     asset[filename(type, options)] = {
